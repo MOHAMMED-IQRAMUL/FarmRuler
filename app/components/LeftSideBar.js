393886e967ef70e4caab7d2d6d53ade46200ceb8
MOHAMMED-IQRAMUL <MOHDIQRAMUL2468@GMAIL.COM>
2024-10-02 11:08:17 +0530

    Adding LeftSideBar component

---
+++ app/components/LeftSideBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LeftSideBar = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default LeftSideBar
--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -11,9 +11,14 @@ export default function Dashboard() {
     const [weather, setWeather] = useState(null);
 
     return (
+        <>
         <div className="min-h-screen flex flex-col">
             <Navbar />
-            <main className="flex-grow container mx-auto p-4">
+
+<div className="flex min-h-screen">
+        <div className="left w-[30%] bg-red-500"></div>
+        <div className="right w-[70%]">
+        <main className="flex-grow container mx-auto p-4">
                 <h1 className="text-3xl font-bold mb-4">Welcome to FarmRuler Dashboard</h1>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                     <div className="bg-white shadow-md rounded-lg p-4 flex items-center">
@@ -74,7 +79,11 @@ export default function Dashboard() {
                     </div>
                 </div>
             </main>
+        </div>
+        </div>
             <Footer />
         </div>
+        </>
+        
     );
 }
