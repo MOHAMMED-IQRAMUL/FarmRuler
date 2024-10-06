6a1290d2bc14b8c2655cad08f51d73c8ca6f8091
harshbhar0629 <harshbhar.0629@gmail.com>
2024-10-06 11:42:34 +0530

    Improve Signin UI

--- app/sign-in/page.js
+++ app/sign-in/page.js
@@ -19,25 +19,25 @@ export default function SignIn() {
                             Sign in to your account
                         </h2>
                     </div>
-                    <form className="mt-8 space-y-6" action="#" method="POST">
+                    <form className="mt-8 space-y-6 border-[2px] border-gray-800 w-[30vw] p-4 rounded-md" action="#" method="POST">
                         <input type="hidden" name="remember" value="true" />
-                        <div className="rounded-md shadow-sm -space-y-px">
-                            <div>
-                                <label htmlFor="email-address" className="sr-only">
+                        <div className="rounded-md shadow-sm -space-y-px flex flex-col justify-center items-center gap-2">
+                            <div className='w-full'>
+                                <label htmlFor="email" className="font-semibold text-[0.92rem]">
                                     Email address
                                 </label>
                                 <input
-                                    id="email-address"
+                                    id="email"
                                     name="email"
                                     type="email"
                                     autoComplete="email"
                                     required
-                                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                                    placeholder="Email address"
+                                    className="appearance-none rounded-md relative block w-full px-3 py-2 border-[2px] border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                                    placeholder="Enter your email"
                                 />
                             </div>
-                            <div>
-                                <label htmlFor="password" className="sr-only">
+                            <div className='w-full'>
+                                <label htmlFor="password" className="font-semibold text-[0.92rem]">
                                     Password
                                 </label>
                                 <input
@@ -46,8 +46,8 @@ export default function SignIn() {
                                     type="password"
                                     autoComplete="current-password"
                                     required
-                                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                                    placeholder="Password"
+                                    className="appearance-none rounded-md relative block w-full px-3 py-2 border-[2px] border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                                    placeholder="Enter your password"
                                 />
                             </div>
                         </div>
