df1598766fea3b5c453866f0ef5ce2239980b030
Sumit Kumar <sumitthakur1600@gmail.com>
2025-01-14 11:54:13 +0000

    Fixed Dashboard

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -40,9 +40,9 @@ return (
             
             <div className="flex min-h-screen">
                 <LeftSideBar />
-                <div className="right w-[70%] min-w-[50%] mt-28">
+                <div className="right w-[70%] min-w-[56%] mt-28">
                     <main className="flex-grow container mx-auto p-4">
-                        <h1 className="text-4xl font-bold mb-4">
+                        <h1 className="text-2xl font-bold mb-4 sm:text-3xl md:text-4xl sm:mb-8">
                             Welcome to FarmRuler Dashboard
                         </h1>
                         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
